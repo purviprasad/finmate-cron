f7262d0026476ab8c58f8a6013fbe7aff84e7715
purviprasad <purviprasadhit@gmail.com>
2023-06-26 02:08:51 +0530

    vercel config change

--- cron/cron.js
+++ cron/cron.js
@@ -29,3 +29,4 @@ exports.cronService = () => {
     }
   );
 };
+cronService();
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const dotenv = require("dotenv");
 dotenv.config({ path: ".env" });
 const express = require("express");
-const { cronService } = require("./cron/cron");
+// const { cronService } = require("./cron/cron");
 const app = express();
-cronService();
+// cronService();
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
