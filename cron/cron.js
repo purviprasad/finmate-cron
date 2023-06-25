4e2138cb6f2fbf3d7a87c1e6da1f1cc3d1a507e0
purviprasad <purviprasadhit@gmail.com>
2023-06-26 02:17:54 +0530

    vercel config change

--- cron/cron.js
+++ cron/cron.js
@@ -8,25 +8,25 @@ const {
   updatePaymentStatus,
   sendPaymentReminderMail,
 } = require("../controllers/UpdatePaymentStatus");
-exports.cronService = () => {
-  cron.schedule(
-    RECURRING_PAYMENT_CRON_SCHEDULE,
-    async () => {
-      try {
-        console.log(
-          "running a task every day to calculate new payment status & send reminder emails" +
-            new Date()
-        );
-        await updatePaymentStatus();
-        await sendPaymentReminderMail();
-      } catch (err) {
-        console.log(err);
-      }
-    },
-    {
-      scheduled: RECURRING_PAYMENT_CRON_START,
-      timezone: RECURRING_PAYMENT_CRON_TIMEZONE,
-    }
-  );
+exports.cronService = async () => {
+  // cron.schedule(
+  //   RECURRING_PAYMENT_CRON_SCHEDULE,
+  //   async () => {
+  try {
+    console.log(
+      "running a task every day to calculate new payment status & send reminder emails" +
+        new Date()
+    );
+    await updatePaymentStatus();
+    await sendPaymentReminderMail();
+  } catch (err) {
+    console.log(err);
+  }
+  // },
+  // {
+  //   scheduled: RECURRING_PAYMENT_CRON_START,
+  //   timezone: RECURRING_PAYMENT_CRON_TIMEZONE,
+  // }
+  // );
 };
 cronService();
